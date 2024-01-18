34ba31bae0b06618dfb0bc28a01317e47ce734e6
Akhilbabu456 <146112110+Akhilbabu456@users.noreply.github.com>
2024-01-18 22:39:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-let screenEl= document.getElementById("screen")
-
-     function input(value){
-      if(value == "clear"){
-        screenEl.innerHTML=""
-      }else if(value == "=" ){
-        screenEl.innerHTML= eval(screenEl.innerHTML)
-      }else{
-        screenEl.innerHTML+= value
-      } 
-    }
+let buttons = document.querySelector('.buttons');
+        let btn = buttons.querySelectorAll('span');
+        let value = document.getElementById('value');
+        for(let i=0 ; i<btn.length ; i++){
+            btn [i] .addEventListener('click'  , function(){
+               if(this.innerHTML == '='){
+                value.innerHTML = eval(value.innerHTML);
+               }else{
+                if(this.innerHTML == 'Clear'){
+                    value.innerHTML = "";
+                }else{
+                    value.innerHTML += this.innerHTML;
+                }
+               }
+            })
+        }
